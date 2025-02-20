897ccbbccb09d46cf8ff01d8f1d4e22697cb9667
arthur790 <arturo.horse90@gmail.com>
2025-02-20 16:52:04 -0600

    validations in form login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useUserContext } from '../context/UserContext'
+import { useNavigate } from 'react-router-dom';
+import { Formik } from 'formik';
+import * as Yup from "yup";
 
 const Login = () => {
+  const { user} = useUserContext();
+  const navigate = useNavigate();
+
+  useEffect(() =>{
+    if (user) navigate("/dashboard")
+  },[user]);
+
+  const onSubmitLogin = async (values, { setSubmitting, setErrors, resetForm }) => {
+    console.log('onSubmit', values);
+
+    //setSubmitting(false);
+  }
+
+  const validationLoginSchema = Yup.object().shape({
+    email: Yup.string().email("el correo no tiene un formato válido").required("El Correo es requerido"),
+    password: Yup.string()
+      .trim()
+      .min(6, "Se requieren minimo 6 carácteres")
+      .required("la Contraseña es requerida"),
+  });
+
   return (
-    <div>Login</div>
+    <>
+      <div>Login</div>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={onSubmitLogin}
+        validationSchema={validationLoginSchema}
+      >
+        {({ handleSubmit, 
+            handleChange, 
+            values, 
+            errors,
+            touched,
+            handleBlur,
+            isSubmitting }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="email"
+              value={values.email}
+              onChange={handleChange}
+              name="email"
+              onBlur={handleBlur}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={values.password}
+              onChange={handleChange}
+              name="password"
+              onBlur={handleBlur}
+            />
+            <button
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Login
+            </button>
+          </form>
+        )}
+      </Formik>
+    </>
+    
   )
 }
 
