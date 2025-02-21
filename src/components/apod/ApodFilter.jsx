a5ba89271c86b1b7d71d8388efcf4703711f79cd
arthur790 <arturo.horse90@gmail.com>
2025-02-21 15:14:22 -0600

    styles in filter

--- src/components/apod/ApodFilter.jsx
+++ src/components/apod/ApodFilter.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const ApodFilter = ({changeFilter}) => {
   return (
-    <div className='grid gap-2'>
-       <h3>Filtra</h3>
-        <input className='w-[90%]'  type="text" name="filter-filter" id="filter-filter"
-         onChange={(e) => changeFilter(e.target.value)}/>
+    <div className='grid gap-2 '>
+        <h3 className="text-sm font-bold">Filtra los resultados:</h3>
+        <div className="flex justify-between">
+          <input   
+          placeholder="filtra por titulo ó por fechas"
+          className="p-4 border-[2px] border-blue-app-600 outline-none 
+          rounded-md flex-1
+          " type="text" name="filter-filter" id="filter-filter"
+          onChange={(e) => changeFilter(e.target.value)}/>
+          <i className="material-symbols-outlined flex-none mt-4 -ml-10 text-blue-app-500">filter_alt</i>
+        </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'material-symbols';
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
