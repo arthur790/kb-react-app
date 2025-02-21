a322d40b93b7bf3fc164d167afa46df3c535531e
arthur790 <arturo.horse90@gmail.com>
2025-02-21 14:05:50 -0600

    styles in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,8 +7,9 @@ const NavBar = () => {
     const { user, setUser} = useUserContext();
 
     return (
-        <div className="bg-white text-black 
-         px-4   top-0 sticky     z-[100] shadow-nav-bar">
+        <div className="bg-gradient-to-r
+           from-blue-app-600  to-blue-app-700 rounded-bl-2xl rounded-br-2xl
+     md:text-white  text-white px-4   top-0 sticky     z-[100] shadow-nav-bar">
 
             <header>
 
@@ -28,21 +29,21 @@ const NavBar = () => {
 
                     <label htmlFor="menu" 
                     className="w-10 h-10 bg-open-menu bg-cover bg-center
-                     bg-green-color rounded-lg  cursor-pointer peer-checked/menu:bg-close-menu 
+                     bg-blue-app-800 rounded-lg  cursor-pointer peer-checked/menu:bg-close-menu 
                      transition-all z-40 md:hidden"></label>
 
                     <ul 
-                    className="fixed inset-0 bg-menu-color 
+                    className="fixed inset-0 bg-blue-app-700 
                             px-[5%] grid gap-6
                             auto-rows-max content-center justify-items-center clip-circle-0 peer-checked/menu:clip-circle-full 
                             transition-[clip-path] duration-500 
                             md:clip-circle-full md:relative md:grid-flow-col md:p-0 md:bg-transparent">
                                 {
                                     user ? <li>
-                                                <NavLink to="/dashboard">Dashboard</NavLink>
+                                                <NavLink to="/dashboard" className="hover:underline">Dashboard</NavLink>
                                             </li> :
                                             <li>
-                                            <NavLink to="/">Home</NavLink>
+                                            <NavLink to="/" className="hover:underline">Home</NavLink>
                                         </li>
                                 }
                        
@@ -53,11 +54,11 @@ const NavBar = () => {
                                 
                                   <>
                                     <li>
-                                    <NavLink to="/login">Login</NavLink>
+                                    <NavLink className="hover:underline" to="/login">Login</NavLink>
                                     </li>
             
                                     <li>
-                                        <NavLink to="/register">Registro</NavLink>
+                                        <NavLink className="hover:underline" to="/register">Registro</NavLink>
                                     </li>
                                 
                                   </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,20 @@ export default {
         "gray-color": '#828282',
         "card-color": '#212121',
         "color-background": '#171717',
-        "menu-color": "#172554"
+        "menu-color": "#172554",
+        'blue-app': {
+              '50': '#ecffff',
+              '100': '#ceffff',
+              '200': '#a3fbfe',
+              '300': '#64f5fc',
+              '400': '#1ee5f2',
+              '500': '#02c9d8',
+              '600': '#04a0b6',
+              '700': '#0b8093',
+              '800': '#136677',
+              '900': '#134f5e',
+              '950': '#073945',
+          },
       },
       backgroundImage: {
         "open-menu": "url('./assets/menu.svg')",
